2b0ddcea98bb20259dc55546e8aa0bfd98570922
Ivan Scherbak <dev@funivan.com>
2018-04-24 08:31:42 +0300

    Rename namespace

--- src/String/StringModfier/StringModifierInterface.php
+++ src/String/StringModfier/StringModifierInterface.php
@@ -2,7 +2,7 @@
 
   declare(strict_types=1);
 
-  namespace Funivan\CabbageCore\String\StringModify;
+  namespace Funivan\CabbageCore\String\StringModfier;
 
   interface StringModifierInterface {
 
